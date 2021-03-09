4fc649ad80d0bf0fc4eb5e555db4519a8ba88030
MateoMiller <64360473+MateoMiller@users.noreply.github.com>
2021-03-09 09:41:48 +0500

    Add files via upload

---
+++ task1/task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new StreamReader("in.txt");
+            var n = int.Parse(file.ReadLine());
+            var edges = new Dictionary<int, List<int>>();
+            for (var i = 1; i <= n; i++)
+            {
+                var input = file.ReadLine().Split();
+                edges.Add(i, input.Select(x => int.Parse(x)).Where(x => x != 0).ToList());
+            }
+            Solve(n, edges);
+        }
+
+        static void Solve(int n, Dictionary<int, List<int>> edges)
+        {
+            var components = new List<List<int>>();
+            var currentComponent = new List<int>();
+            var visited = new HashSet<int>();
+            var stack = new Stack<int>();
+            var currentNode = 1;
+            stack.Push(currentNode);
+            visited.Add(currentNode);
+            currentComponent.Add(currentNode);
+            while (visited.Count != n)
+            {
+                if (stack.Count == 0)
+                {
+                    var nextNode = edges.Keys.First(x => !visited.Contains(x));
+                    components.Add(currentComponent);
+                    currentComponent = new List<int>();
+                    stack.Push(nextNode);
+                    visited.Add(nextNode);
+                    currentComponent.Add(nextNode);
+                }
+                currentNode = stack.Pop();
+                foreach (var node in edges[currentNode].Where(x => !visited.Contains(x)))
+                {
+                    visited.Add(node);
+                    stack.Push(node);
+                    currentComponent.Add(node);
+                }
+            }
+            components.Add(currentComponent);
+            var file = new StreamWriter("out.txt");
+            file.WriteLine(components.Count);
+            components.ForEach(x => file.WriteLine(string.Join(' ', x.OrderBy(x => x))));
+            file.Flush();
+        }
+    }
+}
---
+++ task2/task2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = new StreamReader("in.txt");
+            var n = int.Parse(file.ReadLine());
+            var edges = new Dictionary<int, List<int>>();
+            for (var i = 1; i <= n; i++)
+            {
+                var input = file.ReadLine().Split();
+                var nodes = new List<int>();
+                for (var nodeId = 0; nodeId < n; nodeId += 1)
+                {
+                    if (input[nodeId] != "0")
+                    {
+                        nodes.Add(nodeId + 1);
+                    }
+                }
+                edges.Add(i, nodes);
+            }
+            Solve(n, edges);
+        }
+
+        static void Solve(int n, Dictionary<int, List<int>> edges)
+        {
+            var prev = new Dictionary<int, int>();
+            var queue = new Queue<int>();
+            var currentNode = 1;
+            queue.Enqueue(currentNode);
+            prev.Add(currentNode, -1);
+            while (prev.Keys.Count != n)
+            {
+                if (queue.Count == 0)
+                {
+                    currentNode = edges.Keys.First(x => !prev.ContainsKey(x));
+                    queue.Enqueue(currentNode);
+                    prev.Add(currentNode, -1);
+                }
+                currentNode = queue.Dequeue();
+                foreach (var nextNode in edges[currentNode])
+                {
+                    if (prev.ContainsKey(nextNode))
+                    {
+                        if (prev[currentNode] != nextNode)
+                        {
+                            FindCycle(nextNode, currentNode, prev);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        prev.Add(nextNode, currentNode);
+                        queue.Enqueue(nextNode);
+                    }
+                }
+            }
+            var file = new StreamWriter("out.txt");
+            file.WriteLine("N");
+            file.Flush();
+        }
+
+        static void FindCycle(int firstPoint, int secondPoint ,Dictionary<int, int> prev)
+        {
+            var nodes1 = GetNodesInPath(firstPoint, prev);
+            var nodes2 = GetNodesInPath(secondPoint, prev);
+            var result = nodes1
+                .Concat(nodes2)
+                .Except(nodes1.Intersect(nodes2))
+                .ToHashSet<int>();
+            result.Add(nodes1.First(x => !result.Contains(x)));
+            var file = new StreamWriter("out.txt");
+            file.WriteLine("A");
+            file.WriteLine(string.Join(' ', result.OrderBy(x => x)));
+            file.Flush();
+        }
+
+        static List<int> GetNodesInPath(int lastNode, Dictionary<int, int> prev)
+        {
+            var nodes = new List<int>();
+            while (lastNode != -1)
+            {
+                nodes.Add(lastNode);
+                lastNode = prev[lastNode];
+            }
+            return nodes;
+        }
+    }
+}
